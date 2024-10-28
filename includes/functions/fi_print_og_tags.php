2a19d66d73c7ac6b94c803752042dfad186d3d8b
Pamblam <adelphia@protonmail.ch>
2024-10-27 22:17:14 -0400

    Saving

--- includes/functions/fi_print_og_tags.php
+++ includes/functions/fi_print_og_tags.php
@@ -7,6 +7,9 @@ function fi_print_og_tags(){
 
 	$meta_tags['og:type'] = 'website';
 	if(!empty($GLOBALS['config']->img)) $meta_tags['og:image'] = $GLOBALS['config']->img;
+
+
+
 	if(!empty($GLOBALS['config']->title)) $meta_tags['og:site_name'] = $GLOBALS['config']->title;
 	if(!empty($GLOBALS['config']->desc)) $meta_tags['og:description'] = $GLOBALS['config']->desc;
 
@@ -37,18 +40,19 @@ function fi_print_og_tags(){
 	}
 
 	if(!empty($meta_tags['og:image'])){
-
-		if(strpos($meta_tags['og:image'], fi_get_base_url()) !== 0){
+		if(strpos($meta_tags['og:image'], fi_get_base_url()) !== false){
 			$meta_tags['og:image'] = fi_get_base_url() . ltrim($meta_tags['og:image'], '/');
 		}
 
 		// convert the image URL to local path to get the image size
 		$url_parts = parse_url($meta_tags['og:image']);
+
 		if($url_parts['host'] === $_SERVER['SERVER_ADDR'] || $url_parts['host'] === $_SERVER['HTTP_HOST']){
 			$path = '/' . trim($GLOBALS['config']->base_url, '/');
 			if(strpos($url_parts['path'], $path) === 0){
 				$relative_path = substr($url_parts['path'], strlen($path));
-				list($og_img_width, $og_img_height) = getimagesize(APP_ROOT.'/public'.$relative_path);
+
+				list($og_img_width, $og_img_height) = getimagesize(str_replace('//','/',APP_ROOT.'/public/'.$relative_path));
 				if(!empty($og_img_width) && !empty($og_img_height)){
 					$meta_tags['og:image:width'] = $og_img_width;
 					$meta_tags['og:image:height'] = $og_img_height;
